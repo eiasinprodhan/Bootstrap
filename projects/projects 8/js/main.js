dd55dae1684d022538957ff07c08d921c947fadb
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-04-01 23:10:54 +0600

    Changed

--- projects/projects 8/js/main.js
+++
@@ -1,6 +0,0 @@
-let root = document.getElementById("root");
-let element = document.getElementById("element");
-
-document.addEventListener("scroll",()=>{
-    element.innerHTML = documentElement.scrollTop;
-})
